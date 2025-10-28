6dd565f91f19c43b79904b697b86b4279f285326
halim <halimn007@gmail.com>
2025-10-28 10:07:54 +0530

    npthingh changed

--- cms_backend/doctorbackendapi/urls.py
+++ cms_backend/doctorbackendapi/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ConsultationViewSet, PrescriptionViewSet, LabPrescriptionViewSet
+from .views import ConsultationViewSet, PrescriptionViewSet, LabPrescriptionViewSet  # Import only existing viewsets
 
 router = DefaultRouter()
 router.register(r'consultations', ConsultationViewSet)
